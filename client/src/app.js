d3d9e35b897dc9ce9a5178a8958240c6464d9655
Neville Neville <nchallinger@gmail.com>
2016-07-14 17:09:21 +1000

    added burn details, inc express/mongo routes and seed data.

--- client/src/app.js
+++ client/src/app.js
@@ -1,8 +1,35 @@
 import angular from 'angular'
-angular.module('sissy', [])
-.controller('burnController', function($http){
-	// this.burns = ["soup", "coffee"];
-	$http.get('/burns').then((res) => {
-		this.burns = res.data;
+import 'angular-ui-router'
+angular.module('sissy', ["ui.router"])
+
+.config(($stateProvider, $urlRouterProvider) => {
+	$urlRouterProvider.otherwise('/burns')
+
+	$stateProvider
+	.state('burns', {
+		url: '/burns',
+		templateUrl: 'burns/burns-nav.html',
+		resolve: {
+			burnsService: function($http){
+				return $http.get('/burns');
+			}
+		},
+		controller: function(burnsService){
+			this.burns = burnsService.data;
+		},
+		controllerAs: 'burnCtrl',
+	})
+	.state('burns.details', {
+		url: '/:burnId',
+		templateUrl: 'burns/burns-details.html',
+		resolve: {
+			detailsService: function($http, $stateParams){
+				return $http.get('/burns/' + $stateParams.burnId);
+			}
+		},
+		controller: function(detailsService){
+			this.burnDetails = detailsService.data;
+		},
+		controllerAs: 'detailCtrl',
 	})
 })
--- server/app.js
+++ server/app.js
@@ -11,11 +11,28 @@ app.use( express.static(__dirname + "/../client"));
 app.get("/burns", (req, res) => {
 	let burns = mongoUtil.burns();
 	burns.find().toArray((err, docs) => {
-		// console.log(JSON.stringify(docs));
+		if (err) {
+			res.sendStatus(400);
+		}
 		let burnNames = docs.map((burn) => burn.name);
 		res.json(burnNames);
 	})
-	// res.json(["Soup", "Coffee", "Tea"]);
 });
+
+app.get("/burns/:burnId", (req, res) => {
+
+	let burnId = req.params.burnId;
+	console.log("Burn ID: ", burnId);
+
+	let burns = mongoUtil.burns();
+	burns.find({name: burnId}).limit(1).next((err, docs) => {
+		if (err) {
+			res.sendStatus(400);
+		}
+		//console.log("burn doc: ", docs);
+		res.json(docs);
+	})
+
+})
 
 app.listen(8181, () => console.log("listening on 8181"));

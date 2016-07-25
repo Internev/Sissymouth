07f65307f2fcad2727de042cdb566f2b5e14cab4
Neville Neville <nchallinger@gmail.com>
2016-07-25 16:09:05 +1000

    Cleaner format for form, beautify run on all code

--- client/src/app.js
+++ client/src/app.js
@@ -3,49 +3,55 @@ import 'angular-ui-router'
 angular.module('sissy', ["ui.router"])
 
 .config(($stateProvider, $urlRouterProvider) => {
-	$urlRouterProvider.otherwise('/burns')
+    $urlRouterProvider.otherwise('/burns')
 
-	$stateProvider
-	//Pulls data from server app.js /burns endpoint. 
-	.state('burns', {
-		url: '/burns',
-		templateUrl: 'burns/burns-nav.html',
-		resolve: {
-			burnsService: function($http){
-				return $http.get('/burns');
-			}
-		},
-		controller: function(burnsService){
-			this.burns = burnsService.data;
-			console.log("Data in Angular: ", this.burns);
-		},
-		controllerAs: 'burnCtrl',
-	})
-	.state('burns.details', {
-		url: '/:burnId',
-		templateUrl: 'burns/burns-details.html',
-		resolve: {
-			detailsService: function($http, $stateParams){
-				return $http.get('/burns/' + $stateParams.burnId);
-			}
-		},
-		controller: function(detailsService){
-			this.burnDetails = detailsService.data;
-		},
-		controllerAs: 'detailCtrl',
-	})
-	.state('burns.new', {
-		url: '/add/new',
-		templateUrl: 'burns/new-burn.html',
-		controller: function($state, $http){
-			this.saveBurn = function(burn){
-				$http({method: 'POST', url: '/burns/add/new', data: {burn}})
-				.then(function(){
-					$state.go('burns');
-				});
-				//console.log('burn: ', burn);
-			};
-		},
-		controllerAs: 'newBurnCtrl'
-	})
+    $stateProvider
+    //Pulls data from server app.js /burns endpoint.
+        .state('burns', {
+            url: '/burns',
+            templateUrl: 'burns/burns-nav.html',
+            resolve: {
+                burnsService: function($http) {
+                    return $http.get('/burns');
+                }
+            },
+            controller: function(burnsService) {
+                this.burns = burnsService.data;
+                console.log("Data in Angular: ", this.burns);
+            },
+            controllerAs: 'burnCtrl',
+        })
+        .state('burns.details', {
+            url: '/:burnId',
+            templateUrl: 'burns/burns-details.html',
+            resolve: {
+                detailsService: function($http, $stateParams) {
+                    return $http.get('/burns/' + $stateParams.burnId);
+                }
+            },
+            controller: function(detailsService) {
+                this.burnDetails = detailsService.data;
+            },
+            controllerAs: 'detailCtrl',
+        })
+        .state('burns.new', {
+            url: '/add/new',
+            templateUrl: 'burns/new-burn.html',
+            controller: function($state, $http) {
+                this.saveBurn = function(burn) {
+                    $http({
+                            method: 'POST',
+                            url: '/burns/add/new',
+                            data: {
+                                burn
+                            }
+                        })
+                        .then(function() {
+                            $state.go('burns');
+                        });
+                    //console.log('burn: ', burn);
+                };
+            },
+            controllerAs: 'newBurnCtrl'
+        })
 })
--- server/app.js
+++ server/app.js
@@ -9,42 +9,44 @@ mongoUtil.connect();
 let bodyParser = require('body-parser');
 let jsonParser = bodyParser.json();
 
-app.use( express.static(__dirname + "/../client"));
+app.use(express.static(__dirname + "/../client"));
 
 //Serves the burn object from db to client app.js as json.
 app.get("/burns", (req, res) => {
-	let burns = mongoUtil.burns();
-	burns.find().toArray((err, docs) => {
-		if (err) {
-			res.sendStatus(400);
-		}
-		res.json(docs);
-	})
+    let burns = mongoUtil.burns();
+    burns.find().toArray((err, docs) => {
+        if (err) {
+            res.sendStatus(400);
+        }
+        res.json(docs);
+    })
 });
 
-//Serves the full burn db object, based on search of name. 
+//Serves the full burn db object, based on search of name.
 app.get("/burns/:burnId", (req, res) => {
 
-	let burnId = req.params.burnId;
-	console.log("Burn ID: ", burnId);
+    let burnId = req.params.burnId;
+    console.log("Burn ID: ", burnId);
 
-	let burns = mongoUtil.burns();
-	burns.find({name: burnId}).limit(1).next((err, docs) => {
-		if (err) {
-			res.sendStatus(400);
-		}
-		res.json(docs);
-	})
+    let burns = mongoUtil.burns();
+    burns.find({
+        name: burnId
+    }).limit(1).next((err, docs) => {
+        if (err) {
+            res.sendStatus(400);
+        }
+        res.json(docs);
+    })
 
 });
 
 app.post("/burns/add/new", jsonParser, (request, response) => {
-	let newBurn = request.body.burn || {};
-	let burns = mongoUtil.burns();
+    let newBurn = request.body.burn || {};
+    let burns = mongoUtil.burns();
 
-	burns.save(newBurn);
-	console.log("Burn: ", newBurn);
-	response.sendStatus(201);
+    burns.save(newBurn);
+    console.log("Burn: ", newBurn);
+    response.sendStatus(201);
 });
 
 app.listen(8181, () => console.log("listening on 8181"));
